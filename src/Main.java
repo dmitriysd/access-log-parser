63d8ed25b9fea461d5d842aa5b56a0ebafec525a
DSilkin <dsilkin@inno.tech>
2022-11-25 18:14:36 +0300

    Реализован функционал по сложению, вычитанию, умножению и делению двух чисел

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+
+        System.out.println("Введите первое число и нажмите <Enter>: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число и нажмите <Enter>: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int subtraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        double division = (double)firstNumber / secondNumber;
+
+        System.out.println("Сумма чисел " + firstNumber + " и " + secondNumber + " = " + sum);
+        System.out.println("Разность чисел " + firstNumber + " и " + secondNumber + " = " + subtraction);
+        System.out.println("Произведение чисел " + firstNumber + " и " + secondNumber + " = " + multiplication);
+        System.out.println("Частное чисел " + firstNumber + " и " + secondNumber + " = " + division);
+
     }
 }
